99a40e5cd75549cb8f47cc2694494f3f685936ad
joelusko <joelromerorod6@gmail.com>
2026-05-17 16:00:27 +0200

    Intento de implementar el botón exit

--- src/main.js
+++ src/main.js
@@ -1,1 +1,59 @@
+class EscenaMenu extends Phaser.Scene {
+    constructor() {
+        super({ key: 'EscenaMenu' });
+    }
 
+    preload(){
+
+    }
+
+    create(){
+        
+        const exitButton = this.add.text(600, 600, 'EXIT', {
+            fontSize: '40px',
+            fill: '#ffffff',
+            backgroundColor: '#ba2d2d',
+            padding: { x: 30, y: 15 },
+            fontFamily: 'Arial'
+        });
+
+        exitButton.setOrigin(0.5);
+
+        exitButton.setInteractive({ useHandCursor: true });
+
+        exitButton.on('pointerover', () => {
+            exitButton.setStyle({ fill: '#ba2d2d', backgroundColor: '#ffffff' });
+        });
+
+        exitButton.on('pointerout', () => {
+            exitButton.setStyle({ fill: '#ffffff', backgroundColor: '#ba2d2d' });
+        });
+
+        exitButton.on('pointerdown', () => {
+            this.salirDelJuego();
+        });
+
+
+    }
+
+    update(){
+
+    }
+
+    salirDelJuego(){
+        this.game.destroy(true);
+    }
+
+    
+
+
+}
+const config = {
+    type: Phaser.AUTO,
+    width: 1200,
+    height: 1200,
+    backgroundColor: '#97a3a6',
+    scene: [EscenaMenu]
+};
+
+const game = new Phaser.Game(config);
